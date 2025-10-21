8c70477b843692eb439f75ff9192ca9ecd857431
RoryEiffe <roryeiffe8@gmail.com>
2025-10-21 16:06:47 -0400

    Added functionality to scan markdown files for images, fetch the corresponding images from the original repo, and then send the image data along with the markdown data back to the front-end client.

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(cors());
 
 const PAT = process.env.AZURE_PAT;
 
+console.log(PAT);
+
 app.post('/fetch-azure-file', async (req, res) => {
 
   try {
@@ -31,10 +33,11 @@ app.post('/fetch-azure-file', async (req, res) => {
       return res.status(400).send('File path not found in URL');
     }
 
-    const apiUrl = `https://dev.azure.com/${org}/${project}/_apis/git/repositories/${repo}/items` +
+    const markdownUrl = `https://dev.azure.com/${org}/${project}/_apis/git/repositories/${repo}/items` +
       `?path=${filePath}&includeContent=true&api-version=7.1-preview.1`;
+    
 
-    const response = await fetch(apiUrl, {
+    const response = await fetch(markdownUrl, {
       headers: {
         Authorization: `Basic ${Buffer.from(":" + PAT).toString('base64')}`,
         Accept: 'application/json'
@@ -47,7 +50,26 @@ app.post('/fetch-azure-file', async (req, res) => {
 
     const content = await response.text();
 
-    res.send({ content });
+    const imgsMetaData = await img_util(content, markdownUrl);
+    let imgs = [];
+
+    for (let img of imgsMetaData) {
+      // fetch image from url:
+        const imgResponse = await fetch(img.url, {
+          headers: {
+            Authorization: `Basic ${Buffer.from(":" + PAT).toString('base64')}`,
+            Accept: 'application/octet-stream'
+          }
+        });
+        if (!imgResponse.ok) {
+          return res.status(imgResponse.status).send('Failed to fetch image content');
+        }
+        const imgBuffer = await imgResponse.arrayBuffer();
+        const imgData = Buffer.from(imgBuffer).toString('base64');
+        imgs.push({imgData, name: img.name});
+    }
+
+    res.send({ content, imgs });
 
   }
 
@@ -58,5 +80,19 @@ app.post('/fetch-azure-file', async (req, res) => {
 
 
 });
+
+const img_util = async (content, markdownURL) => {
+  // Search for all instances of image links in markdown format ![alt text](image_url)
+  const imgRegex = /!\[.*?\]\((.*?)\)/g;
+  const imgs = [];
+  let match;
+  while ((match = imgRegex.exec(content)) !== null) {
+    const imagePath = match[1];
+    const imageApiUrl = markdownURL.replace('Cumulative.md', imagePath) + '&api-version=7.1-preview.1&$format=octetStream';
+
+    imgs.push({url: imageApiUrl, name: imagePath});
+  }
+  return imgs;
+}
 
 app.listen(3001, () => console.log("Server running on port 3001"));
